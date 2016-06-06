2922c7e20af4ab4e11ab5d46153c7a0a2c68da83
David Davidson <david.davidson@formidable.com>
2016-06-06 08:46:13 -0700

    Add attribution

--- js/footer.jsx
+++ js/footer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
+      <p>Photos by <a href="https://www.flickr.com/photos/gageskidmore/">Gage Skidmore</a></p>
       <p>Paid for by American Innovators for Donald J. Trump</p>
     </footer>
   )
